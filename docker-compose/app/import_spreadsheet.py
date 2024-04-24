0843a80016fd341ef44971ec3efdca94fc722460
Lucas Garcia <lucas.garcia@glovoapp.com>
2024-04-24 15:17:47 +0200

    Reorganizing files

--- docker-compose/app/import_spreadsheet.py
+++ docker-compose/app/import_spreadsheet.py
@@ -7,9 +7,9 @@ from spreadsheet_client import GoogleSheetsReader
 
 
 APP_PATH = f"{os.getenv('HOME')}/Github/parfumvault/docker-compose/app"
-SPREADSHEET_CREDENTIALS_FILE = f"{APP_PATH}/spreadsheet_credentials.json"
-DB_CREDENTIALS_FILE = f"{APP_PATH}/db_credentials.json"
-MATERIALS_FILE = f"{APP_PATH}/materials.json"
+SPREADSHEET_CREDENTIALS_FILE = f"{APP_PATH}/credentials/spreadsheet_credentials.json"
+DB_CREDENTIALS_FILE = f"{APP_PATH}/credentials/db_credentials.json"
+MATERIALS_FILE = f"{APP_PATH}/artifacts/materials.json"
 
 
 def db_test():
